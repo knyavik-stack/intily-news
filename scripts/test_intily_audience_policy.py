3367147ff69c2ac8383d28b4ef0f99a22343b20d
knyavik <knyavik@gmail.com>
2026-09-07 11:14:02 +0300

    test(scoring): verify linear audience bonus

--- scripts/test_intily_audience_policy.py
+++ scripts/test_intily_audience_policy.py
@@ -8,10 +8,11 @@ class AudiencePolicyTests(unittest.TestCase):
         self.assertEqual(PRE_AI_THRESHOLD, 45.0)
         self.assertEqual(FINAL_THRESHOLD, 60.0)
 
-    def test_bonus_is_positive_only_above_neutral_fit(self):
-        self.assertEqual(bonus_from_score(5), 0.0)
-        self.assertEqual(bonus_from_score(6), 3.0)
-        self.assertEqual(bonus_from_score(10), 15.0)
+    def test_bonus_is_linear_from_one_to_ten(self):
+        self.assertEqual(
+            [bonus_from_score(i) for i in range(1, 11)],
+            [2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0],
+        )
 
     def test_range_is_strict(self):
         self.assertEqual(clamp_score(8), 8.0)
