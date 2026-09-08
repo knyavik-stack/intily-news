1dafe79e2217a55bf03d3031810a4e73648c1947
knyavik <knyavik@gmail.com>
2026-09-08 15:59:42 +0300

    Update test_intily_audience_policy.py

--- scripts/test_intily_audience_policy.py
+++ scripts/test_intily_audience_policy.py
@@ -9,10 +9,10 @@ class AudiencePolicyTests(unittest.TestCase):
         self.assertEqual(FINAL_THRESHOLD, 55.0)
 
     def test_ai_layer_is_exactly_thirty_points(self):
-        self.assertEqual(AUDIENCE_BONUS_MAX, 50.0)
+        self.assertEqual(AUDIENCE_BONUS_MAX, 30.0)
         self.assertEqual(
             [bonus_from_score(i) for i in range(1, 11)],
-            [5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0],
+            [21.0, 22.0, 23.0, 24.0, 25.0, 26.0, 27.0, 28.0, 29.0, 30.0],
         )
 
     def test_range_is_strict(self):
